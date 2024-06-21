bf998cc68f1868a854125d431adf2d8a3f62bc53
João VIctor Silva <joaovictordes02@gmail.com>
2024-06-21 00:55:38 -0300

    Exercicio dia 4 - Verificação de idade

---
+++ dia_4/ex01.js
@@ -0,0 +1,12 @@
+// Condicional - Escopo - Operadores
+
+// Verificando Idade 
+
+let idadeUsuario = Number(prompt("Qual a sua idade?"))
+
+if(idadeUsuario >= 18){
+    console.log("Você é maior de idade")
+}else{
+    console.log("Você é menor de idade")
+}
+

b9204b4877367d0f8b5f63a12c62146ec08a5f7d
João VIctor Silva <joaovictordes02@gmail.com>
2024-06-22 01:03:16 -0300

    Desafios dia 06

---
+++ ex01.js
@@ -0,0 +1,9 @@
+// Aprendendo FOR 
+
+// Tabuada
+
+// let tabuada = Number(prompt("Escolha a tabuada desejada: "))
+
+// for(let multiplicador = 0; multiplicador <= 100; multiplicador++){
+//     console.log(`${tabuada} * ${multiplicador} =`, tabuada*multiplicador)
+// }

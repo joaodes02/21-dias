b5ef541f2f32843b6b956692663009bb649b3b3a
João VIctor Silva <joaovictordes02@gmail.com>
2024-06-27 23:01:36 -0300

    Continuação Lista de Ex - Arrays

--- dia_10/index.js
+++ dia_10/index.js
@@ -3,22 +3,46 @@
 //Popule o array e no final exiba os índices em que número inserido aparece nesse array. 
 //Para testes do exercício use até 10 elementos no array.
 
-const array = [];
-const indices = [];
-let contador = 0
+// const array = [];
+// const indices = [];
+// let contador = 0
 
-for(let i = 0; i < 10; i++){
-    const elemento = parseInt(prompt(`Insira o elemento ${i+1}: `));
-    array[i] = elemento;
+// for(let i = 0; i < 10; i++){
+//     const elemento = parseInt(prompt(`Insira o elemento ${i+1}: `));
+//     array[i] = elemento;
+// }
+
+// const numeroProcurado = parseInt(prompt("Insira o numero procurado: "))
+
+// for(let i = 0; i < 10; i++){
+//     if(array[i] === numeroProcurado){
+//         indices[contador] = i 
+//         contador++
+//     }
+// }
+
+// console.log(` numero ${numeroProcurado} foi encotrando nos indices: ${indices}`);
+
+// 2 - Crie um algoritmo que peça 5 números ao usuário, 
+// coloque eles em um array, depois exiba esse array. 
+// Modifique os elementos do array de modo que a sequência de números fique ao contrário.
+
+
+let array= []
+let arrayInvertido = []
+
+for(let i =0; i < 5; i++ ){
+    let numeros = prompt(`Insire o elemento ${i + 1}: `)
+    array[i] = [numeros]
 }
 
-const numeroProcurado = parseInt(prompt("Insira o numero procurado: "))
+console.log(`Array Original: ${array}`)
 
-for(let i = 0; i < 10; i++){
-    if(array[i] === numeroProcurado){
-        indices[contador] = i 
-        contador++
-    }
+let contador = 4
+
+for(let i = 0; i<5; i++){
+    arrayInvertido[i] = array[contador]
+    contador--
 }
 
-console.log(` numero ${numeroProcurado} foi encotrando nos indices: ${indices}`);
+console.log(`Array Invertido: ${arrayInvertido}`)
